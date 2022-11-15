00cdf61820586832759b659fcf0115faccde56b2
Adrian Barrero <adribf4@gmail.com>
2022-11-15 12:23:49 +0100

    test

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Curso-JavaScript
-# Curso-JavaSript
+
--- SECCION 11/app.js
+++ SECCION 11/app.js
@@ -1,9 +1,3 @@
-const gitExtension = vscode.extensions.getExtension<gitExtension>('vscode.git').exports;
-const git = gitExtension.getAPI(1);
-
 const ceviche = ['Pescado','Mariscos','Cebolla'];
 ceviche.splice(1,0,'liomn');
 console.log(ceviche);
-
-let test = true;
-console.log(test);
