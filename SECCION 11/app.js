8c6a90a444ec38e871ea5bfa9052f3595c419e4f
Adrian Barrero <adribf4@gmail.com>
2022-11-15 13:41:43 +0100

    Fin Seccion 11

--- SECCION 11/app.js
+++ SECCION 11/app.js
@@ -1,7 +1,67 @@
 const ceviche = ['Pescado','Mariscos','Cebolla'];
-ceviche.splice(1,0,'liomn');
-console.log(ceviche);
+const ingredientes= [
+    {
+        codigo: 1,
+        titulo: 'pescado'
+    },
+    {
+        codigo: 2,
+        titulo: 'mariscos'
+    },
+    {
+        codigo: 3,
+        titulo: 'cebolla'
+    }
+]
+// ceviche.splice(1,0,'liomn');
+// console.log(ceviche);
 
-ceviche.forEach(function(){
-    console.log('Test');
+// /******************
+//  * RECORRER ARRAYS
+//  ******************/
+// ceviche.forEach(function(item, index){
+//     console.log(`Index ${index}: ${item}`);
+// });
+
+// for(let i=0;i < ceviche.length;i++){
+//     console.log(`Index ${i}: ${ceviche[i]}`);
+// }
+
+/******************************
+ * BUSCAR EN ARRAY
+ ******************************/
+// const index = ingredientes.findIndex(function(ing, index){
+//     return ing.titulo === 'cebolla';
+// });
+
+// console.log(index);
+//console.log(ceviche.indexOf('pescado'));
+
+// const buscarIngrediente = function(ingredientes, titulo){
+//     const index = ingredientes.findIndex(function(ing, index){
+//         return ing.titulo === titulo;
+//     });
+//     return index;
+// }
+
+// const filtrarIngredientes = ingredientes.filter(function(ing,index){
+//     const ingredientes = ing.titulo.includes('mar');
+//     return ingredientes;
+// }); 
+// console.log(filtrarIngredientes);
+//console.log(ingredientes[buscarIngrediente(ingredientes,'pescado')]);
+
+// const paises =['Colombia','Ecuador','Perú','España','Mexico'];
+// console.log(paises.filter(pais => pais.includes('Es')));
+ingredientes.sort(function(a,b){
+    if(a.titulo > b.titulo){
+        return 1;
+    }else if (a.titulo < b.titulo){
+        return -1;
+    } else {
+        return 0;
+    }
+
 });
+
+console.log(ingredientes);
